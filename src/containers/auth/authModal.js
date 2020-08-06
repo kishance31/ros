d8c9a54944e53bf0d502d3bc19453c60ec17317e
CZkishan <kishan.makwana@codezeros.com>
2020-08-06 10:29:36 +0530

    fix: resolve conflict

--- src/containers/auth/authModal.js
+++ src/containers/auth/authModal.js
@@ -3,11 +3,9 @@ import ModalComponent from '../../components/modal/modal';
 import SignInForm from './signInForm';
 import SignUpForm from './signUpForm';
 import { authModalConstants } from '../../utils/constants';
-<<<<<<< HEAD
 import FormFooter from '../footer/formFooter';
-=======
-//import { ModalFooter } from 'reactstrap';
->>>>>>> c058ada85fecf311364d29dcaec17da24b6fdf5b
+import { ModalFooter } from 'reactstrap';
+
 
 const AuthModalContainer = props => {
 
@@ -27,16 +25,14 @@ const AuthModalContainer = props => {
         }
         if (title === authModalConstants.SIGN_UP_TITLE) {
             return (
-<<<<<<< HEAD
                 <div>
                 <FormFooter footertitle="Already have an account!" footerToNavigate="SIGN IN" />
-=======
                 <div class="modal-footer">
                     <h5 class="footer_title"> Already have an account!</h5>
                     <span class="navbar-text"> 
                         <a href="#" data-dismiss="modal" data-toggle="modal" data-target="#SigninModalCenter">SIGN IN</a> 
                     </span>
->>>>>>> c058ada85fecf311364d29dcaec17da24b6fdf5b
+                </div>
                 </div>
             )
         }
