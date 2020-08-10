b9086cf0771a6eb8053fc4975d9aae8df26d4f42
hCodezeros <himani.patel@codezeros.com>
2020-08-10 13:47:52 +0530

    changes in form footer

--- src/containers/auth/uploadDoc.js
+++ src/containers/auth/uploadDoc.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 
 class UploadDoc extends Component {
 
@@ -27,7 +27,7 @@ class UploadDoc extends Component {
     render() {
 
         return (
-            <div className="upload_document_file">
+            <div className="upload_document_file" onClick={this.onFileUpload} >
                 <svg xmlns="http://www.w3.org/2000/svg" width="81" height="56" viewBox="0 0 81 56">
                     <g id="Group_1341" data-name="Group 1341" transform="translate(-641 -735)">
                         <g id="Rectangle_20" data-name="Rectangle 20" transform="translate(641 735)" fill="#f8f8f8" stroke="#bfbfbf" stroke-width="1">
@@ -37,12 +37,11 @@ class UploadDoc extends Component {
                         <path id="Icon_ionic-md-add" data-name="Icon ionic-md-add" d="M29.25,19.5H19.5v9.75h-3V19.5H6.75v-3H16.5V6.75h3V16.5h9.75Z" transform="translate(699.438 744.805) rotate(90)" fill="#292929" />
                     </g>
                 </svg>
-                <label onClick={this.onFileUpload}
-                       className="upload_document" for="inputGroupFile03">UPLOAD DOCUMENT</label>
+                <label className="upload_document" for="inputGroupFile03">UPLOAD DOCUMENT</label>
                 <input type="file"
                     className="custom-file-input"
                     id="inputGroupFile03"
-                    onChange={this.onFileChange} />
+                    onChange={this.onFileChange} required />
             </div>
 
         );
@@ -50,3 +49,4 @@ class UploadDoc extends Component {
 } 
 
 export default UploadDoc;
+
--- src/containers/footer/formFooter.js
+++ src/containers/footer/formFooter.js
@@ -1,28 +1,13 @@
 import React from 'react';
-import AuthModelAction from '../../actions/auth.action';
-import { AuthMap } from '../../actions/auth.action';
-import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 
 const FormFooter = (props) => {
-
-    const { footertitle, footerToNavigate, footertitle1, footerToNavigate1 } = props;
-
-    const dispatch = useDispatch();
-    
-    const navigateToSignIn = () => {
-        dispatch(AuthModelAction.toggleAuthModals(AuthMap.TOGGLE_SIGN_IN_MODAL));
-    }
-
+    const {footertitle, footerToNavigate} = props;
     return (
         <>
             <div className="modal-footer">
                 <h5 className="footer_title"> {footertitle} </h5>
                 <span className="navbar-text"> <a href="#/" data-toggle="modal" data-target="#SignupModalCenter" data-dismiss="modal">{footerToNavigate}</a> </span>
-            </div>
-            <div class="modal-footer">
-                <h5 class="footer_title">{footertitle1}</h5>
-                <span class="navbar-text"><a onClick={navigateToSignIn} href="#" data-dismiss="modal" data-toggle="modal" data-target="#SigninModalCenter">{footerToNavigate1}</a></span>
             </div>
         </>)
 }
@@ -31,8 +16,5 @@ export default FormFooter;
 
 FormFooter.prototype = {
     footertitle: PropTypes.string,
-    footerToNavigate: PropTypes.string,
-    footertitle1: PropTypes.string,
-    footerToNavigate1: PropTypes.string
-
+    footerToNavigate: PropTypes.string
 }
