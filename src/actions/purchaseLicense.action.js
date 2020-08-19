5f023053435d36befd2fefe2edfc75027a0517b8
hCodezeros <himani.patel@codezeros.com>
2020-08-19 17:27:29 +0530

    Did some changes in Api integration.

--- src/actions/purchaseLicense.action.js
+++ src/actions/purchaseLicense.action.js
@@ -5,7 +5,13 @@ export const PurchaseLicenseMap = {
     AVAILABLE_LICENSE_START: 'available_license_start',
     AVAILABLE_LICENSE_SUCCESS: 'available_license_success',
     AVAILABLE_LICENSE_ERROR: 'available_license_error',
-
+    ORDER_ID_START: 'order_id_start',
+    ORDER_ID_SUCCESS: 'order_id_success',
+    ORDER_ID_ERROR: 'order_id_error',
+    PURCHASE_LICENSE_START:'purchase_license_start',
+    PURCHASE_LICENSE_SUCCESS:'purchase_license_success',
+    PURCHASE_LICENSE_ERROR:'purchase_license_error'
+    
 }
 
 const purchaseLicenseAction = {
@@ -17,7 +23,7 @@ const purchaseLicenseAction = {
     }
 }
 
-export const availableLicenseAsync =() => {
+export const availableLicenseAsync = () => {
 
     return async (dispatch) => {
 
@@ -34,7 +40,7 @@ export const availableLicenseAsync =() => {
         if (availableLicenseResponse.data.response.responseCode === 200) {
             dispatch({
                 type: PurchaseLicenseMap.AVAILABLE_LICENSE_SUCCESS,
-                payload:availableLicenseResponse.data.response.licenseList
+                payload: availableLicenseResponse.data.response.licenseList
             })
         } else {
             dispatch({
@@ -43,4 +49,66 @@ export const availableLicenseAsync =() => {
         }
     }
 }
+
+export const orderIdAsync = (orderId,token) => {
+
+    return async (dispatch) => {
+
+        dispatch({
+            type: PurchaseLicenseMap.ORDER_ID_START
+        });
+        console.log(orderId);
+        console.log(token);
+        let orderIdResponse = await axios({
+            url: `http://127.0.0.1:4000/api/corporate-admin/purchaseLicense/orderId/${orderId}`,
+            method: "POST",
+            headers: {
+                'tokens': token
+            }
+        });
+        console.log(orderIdResponse)
+        if (orderIdResponse.data.response.responseCode === 201) {
+            dispatch({
+                type: PurchaseLicenseMap.ORDER_ID_SUCCESS,
+                payload: orderIdResponse.data.response.orderDetails.orderId
+            })
+        } else {
+            dispatch({
+                type: PurchaseLicenseMap.ORDER_ID_ERROR
+            })
+        }
+    }
+}
+
+export const purchaseLicenseAsync = (orderId, purchaseLicenses, tokens) => {
+
+    return async (dispatch) => {
+
+        dispatch({
+            type: PurchaseLicenseMap.PURCHASE_LICENSE_START
+        });
+        let purchaseLicenseResponse = await axios({
+            url: `http://127.0.0.1:4000/api/corporate-admin/purchaseLicense/purchase`,
+            method: "POST",
+            data:{
+                orderId, purchaseLicenses
+            },
+            headers: {
+                'Content-type': 'application/json',
+                tokens
+            }
+        });
+        if (purchaseLicenseResponse.data.response.responseCode === 201) {
+            dispatch({
+                type: PurchaseLicenseMap.PURCHASE_LICENSE_SUCCESS
+            })
+        } else {
+            dispatch({
+                type: PurchaseLicenseMap.PURCHASE_LICENSE_ERROR
+            })
+        }
+    }
+}
+
+
 export default purchaseLicenseAction;
