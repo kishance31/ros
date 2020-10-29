c5563e6cf266c79033f17bf8e54c7bbfc0f9365c
hCodezeros <himani.patel@codezeros.com>
2020-10-29 13:47:18 +0530

    API call in contact us form

--- src/pages/Homepage/contactUs.js
+++ src/pages/Homepage/contactUs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { ContactusLinks } from "../../utils/constants";
+import ContactUsForm from './contactUsForm';
 
 const ContactUs = () => {
     const data = useSelector(state => state.homepageContent.contactUsDetais)
@@ -12,37 +13,29 @@ const ContactUs = () => {
                         <h2 className="title">CONTACT US</h2>
                         <h6>Get In Touch</h6>
                         {data
-                    ? <>
-                        <p>
-                            <a>
-                                <b>A: &nbsp;</b>{data.address}
-                            </a>
-                        </p>
-                        <p>
-                            <a>
-                                <b>E: &nbsp;</b>{data.email}
-                            </a>
-                        </p>
-                        <p>
-                            <a>
-                                <b>P: &nbsp;</b>{data.contact}
-                            </a>
-                        </p>
-                    </> : []
-                }
+                            ? <>
+                                <p>
+                                    <a>
+                                        <b>A: &nbsp;</b>{data.address}
+                                    </a>
+                                </p>
+                                <p>
+                                    <a>
+                                        <b>E: &nbsp;</b>{data.email}
+                                    </a>
+                                </p>
+                                <p>
+                                    <a>
+                                        <b>P: &nbsp;</b>{data.contact}
+                                    </a>
+                                </p>
+                            </> : []
+                        }
                     </div>
                 </div>
                 <div className="col-lg-5 col-md-12">
                     <div className="news_letter">
-                        <div>
-                            <form>
-                                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="your fullname" />
-                                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="your email address" />
-                                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="mobile number" />
-                                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="comment" />
-                            </form>
-                            <p><a className="btn news_letter_btn btn-lg" href="#" role="button">SEND</a></p>
-                        </div>
+                        <ContactUsForm />
                     </div>
                 </div>
                 <div className="col-lg-3 col-md-12">
