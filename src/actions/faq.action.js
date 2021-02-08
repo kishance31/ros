2040beb2f853c8d2aacbadc86b3eb78865d6784e
hCodezeros <himani.patel@codezeros.com>
2021-02-08 12:10:55 +0530

    integration in social media links

--- src/actions/faq.action.js
+++ src/actions/faq.action.js
@@ -8,6 +8,9 @@ export const FAQMap = {
     GET_FAQS_START: 'GET_FAQS_START',
     GET_FAQS_SUCCESS: 'GET_FAQS_SUCCESS',
     GET_FAQS_ERROR: 'GET_FAQS_ERROR',
+    GET_SOCIAL_LINKS_START: 'GET_SOCIAL_LINKS_START',
+    GET_SOCIAL_LINKS_SUCCESS: 'GET_SOCIAL_LINKS_SUCCESS',
+    GET_SOCIAL_LINKS_ERROR: 'GET_SOCIAL_LINKS_ERROR',
 }
 
 export const getFAQSAsync = () => {
@@ -32,6 +35,34 @@ export const getFAQSAsync = () => {
         } catch (error) {
             dispatch({
                 type: FAQMap.GET_FAQS_ERROR
+            })
+            dispatch(notificationActions('error', "Error while updating", 3000))
+        }
+    }
+}
+
+export const getSocialLinksAsync = () => {
+    return async (dispatch) => {
+        try {
+            dispatch({
+                type: FAQMap.GET_SOCIAL_LINKS_START
+            });
+            let { data } = await axios({
+                url: `${serverUrl}/admin/cms/getSocialMediaLinks`,
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            if (data.response && data.response.responseCode === 200) {
+                return dispatch({
+                    type: FAQMap.GET_SOCIAL_LINKS_SUCCESS,
+                    payload: data.response.data
+                })
+            }
+        } catch (error) {
+            dispatch({
+                type: FAQMap.GET_SOCIAL_LINKS_ERROR
             })
             dispatch(notificationActions('error', "Error while updating", 3000))
         }
